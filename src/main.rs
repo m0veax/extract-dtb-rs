966aff41d71855c5eac0b8921d343d9ff12ae6ff
m0veax <patrick.kilter@gmail.com>
2024-01-26 22:46:51 +0100

    split library to extract dtbs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
 use clap::Parser;
-
-/*
-
-def main():
-    parser = argparse.ArgumentParser(description="Extract dtbs from kernel images.")
-    parser.add_argument("filename", help="Android kernel image")
-    parser.add_argument(
-        "-o", dest="output_dir", default="dtb", required=False, help="Output directory"
-    )
-    parser.add_argument(
-        "-n",
-        dest="extract",
-        action="store_false",
-        default=True,
-        required=False,
-        help="Do not extract, just output information",
-    )
-    parser.add_argument("-V", "--version", action="version", version=__version__)
-
-    args = parser.parse_args() */
+mod split;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
